3058327bbb9a25ad48ef1031a79f8f1463ac45b1
Vukul <vu@cumallover.me>
2017-03-27 10:27:22 +0200

    Pablofix

--- GruppeoppgavexD/Form1.Designer.cs
+++ GruppeoppgavexD/Form1.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.Bildested = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.Alt1 = new System.Windows.Forms.RadioButton();
-            this.Alt2 = new System.Windows.Forms.RadioButton();
-            this.Alt3 = new System.Windows.Forms.RadioButton();
-            this.Neste = new System.Windows.Forms.Button();
-            this.Sjekk = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.Sjekk = new System.Windows.Forms.Button();
+            this.Neste = new System.Windows.Forms.Button();
+            this.Alt3 = new System.Windows.Forms.RadioButton();
+            this.Alt2 = new System.Windows.Forms.RadioButton();
+            this.Alt1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Bildested)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -45,8 +45,9 @@
             // 
             this.Bildested.Dock = System.Windows.Forms.DockStyle.Left;
             this.Bildested.Location = new System.Drawing.Point(0, 0);
+            this.Bildested.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Bildested.Name = "Bildested";
-            this.Bildested.Size = new System.Drawing.Size(816, 660);
+            this.Bildested.Size = new System.Drawing.Size(1451, 1023);
             this.Bildested.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Bildested.TabIndex = 0;
             this.Bildested.TabStop = false;
@@ -61,93 +62,102 @@
             this.groupBox1.Controls.Add(this.Alt1);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.groupBox1.Location = new System.Drawing.Point(816, 0);
+            this.groupBox1.Location = new System.Drawing.Point(1451, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(432, 660);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.groupBox1.Size = new System.Drawing.Size(768, 1023);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Spørsmål 1";
-            // 
-            // Alt1
-            // 
-            this.Alt1.AutoSize = true;
-            this.Alt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Alt1.Location = new System.Drawing.Point(11, 103);
-            this.Alt1.Name = "Alt1";
-            this.Alt1.Size = new System.Drawing.Size(21, 20);
-            this.Alt1.TabIndex = 1;
-            this.Alt1.TabStop = true;
-            this.Alt1.UseVisualStyleBackColor = true;
-            // 
-            // Alt2
-            // 
-            this.Alt2.AutoSize = true;
-            this.Alt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Alt2.Location = new System.Drawing.Point(11, 147);
-            this.Alt2.Name = "Alt2";
-            this.Alt2.Size = new System.Drawing.Size(21, 20);
-            this.Alt2.TabIndex = 2;
-            this.Alt2.TabStop = true;
-            this.Alt2.UseVisualStyleBackColor = true;
-            // 
-            // Alt3
-            // 
-            this.Alt3.AutoSize = true;
-            this.Alt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Alt3.Location = new System.Drawing.Point(11, 191);
-            this.Alt3.Name = "Alt3";
-            this.Alt3.Size = new System.Drawing.Size(21, 20);
-            this.Alt3.TabIndex = 3;
-            this.Alt3.TabStop = true;
-            this.Alt3.UseVisualStyleBackColor = true;
-            // 
-            // Neste
-            // 
-            this.Neste.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Neste.Location = new System.Drawing.Point(108, 440);
-            this.Neste.Name = "Neste";
-            this.Neste.Size = new System.Drawing.Size(140, 49);
-            this.Neste.TabIndex = 4;
-            this.Neste.Text = "Neste";
-            this.Neste.UseVisualStyleBackColor = true;
-            // 
-            // Sjekk
-            // 
-            this.Sjekk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Sjekk.Location = new System.Drawing.Point(108, 508);
-            this.Sjekk.Name = "Sjekk";
-            this.Sjekk.Size = new System.Drawing.Size(140, 49);
-            this.Sjekk.TabIndex = 5;
-            this.Sjekk.Text = "Sjekk svar";
-            this.Sjekk.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 57);
+            this.label2.Location = new System.Drawing.Point(11, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(215, 25);
+            this.label2.Size = new System.Drawing.Size(358, 39);
             this.label2.TabIndex = 6;
             this.label2.Text = "Hvilket land er dette?";
             // 
+            // Sjekk
+            // 
+            this.Sjekk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sjekk.Location = new System.Drawing.Point(192, 787);
+            this.Sjekk.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Sjekk.Name = "Sjekk";
+            this.Sjekk.Size = new System.Drawing.Size(249, 76);
+            this.Sjekk.TabIndex = 5;
+            this.Sjekk.Text = "Sjekk svar";
+            this.Sjekk.UseVisualStyleBackColor = true;
+            // 
+            // Neste
+            // 
+            this.Neste.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Neste.Location = new System.Drawing.Point(192, 682);
+            this.Neste.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Neste.Name = "Neste";
+            this.Neste.Size = new System.Drawing.Size(249, 76);
+            this.Neste.TabIndex = 4;
+            this.Neste.Text = "Neste";
+            this.Neste.UseVisualStyleBackColor = true;
+            // 
+            // Alt3
+            // 
+            this.Alt3.AutoSize = true;
+            this.Alt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Alt3.Location = new System.Drawing.Point(20, 296);
+            this.Alt3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Alt3.Name = "Alt3";
+            this.Alt3.Size = new System.Drawing.Size(33, 32);
+            this.Alt3.TabIndex = 3;
+            this.Alt3.TabStop = true;
+            this.Alt3.UseVisualStyleBackColor = true;
+            // 
+            // Alt2
+            // 
+            this.Alt2.AutoSize = true;
+            this.Alt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Alt2.Location = new System.Drawing.Point(20, 228);
+            this.Alt2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Alt2.Name = "Alt2";
+            this.Alt2.Size = new System.Drawing.Size(33, 32);
+            this.Alt2.TabIndex = 2;
+            this.Alt2.TabStop = true;
+            this.Alt2.UseVisualStyleBackColor = true;
+            // 
+            // Alt1
+            // 
+            this.Alt1.AutoSize = true;
+            this.Alt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Alt1.Location = new System.Drawing.Point(20, 160);
+            this.Alt1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Alt1.Name = "Alt1";
+            this.Alt1.Size = new System.Drawing.Size(33, 32);
+            this.Alt1.TabIndex = 1;
+            this.Alt1.TabStop = true;
+            this.Alt1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Spørsmål 1";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1248, 660);
+            this.ClientSize = new System.Drawing.Size(2219, 1023);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Bildested);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
